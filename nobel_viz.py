9e8e8a08363ea88074e2880f515f9dbcdab5b084
podberezsky <podberezsky@my.com>
2020-07-04 13:43:57 +0300

    First commit

---
+++ nobel_viz.py
@@ -0,0 +1,11 @@
+# nobel_viz.py 
+from flask import Flask 
+
+app = Flask(__name__)  
+
+@app.route("/") 
+def hello():  
+    return "Hello World!"  
+
+if __name__ == "__main__":  
+    app.run(port=8000, debug=True) 
