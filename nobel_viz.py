deee9f8a5f426f09b6b21b8a85aa3dc97dc17528
podberezsky <podberezsky@my.com>
2020-07-04 15:45:10 +0300

    Добавлен шаблон

--- nobel_viz.py
+++ nobel_viz.py
@@ -1,11 +1,21 @@
 # nobel_viz.py 
-from flask import Flask 
+from flask import Flask, render_template 
 
-app = Flask(__name__)  
+app = Flask(__name__) 
+
+winners = [  
+        {'name': 'Albert Einstein', 'category':'Physics'},  
+        {'name': 'V.S. Naipaul', 'category':'Literature'},  
+        {'name': 'Dorothy Hodgkin', 'category':'Chemistry'} 
+        ]
 
 @app.route("/") 
 def hello():  
-    return "Hello World!"  
+    return "Hello World!" 
+
+@app.route('/demolist') 
+def demo_list():  
+    return render_template('testj2.html',  heading="A little winners' list",  winners=winners  )
 
 if __name__ == "__main__":  
     app.run(port=8000, debug=True) 
