0aedd030d5e2dd6b2706e6079b1cb06e5c286685
podberezsky <podberezsky@my.com>
2020-07-05 15:13:01 +0300

    Первый пример самопального сервера

---
+++ viz/script.js
@@ -0,0 +1,7 @@
+d3.json('data/nobel_winners_cleaned.json', function(error, data){   if(error){  
+	console.log(error);  
+}   
+d3.select('h2#data-title').text('All the Nobel-winners');  
+
+d3.select('div#data pre').html(JSON.stringify(data, null, 4)); 
+});
