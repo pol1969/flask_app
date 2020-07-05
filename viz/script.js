84147a1036498f9b642fefe19d7bdf1af5c9dafe
podberezsky <podberezsky@my.com>
2020-07-05 15:48:49 +0300

    Разбиение большого json на части, переписана фунция его загрузки по названию страны

--- viz/script.js
+++ viz/script.js
@@ -1,7 +1,10 @@
-d3.json('data/nobel_winners_cleaned.json', function(error, data){   if(error){  
-	console.log(error);  
-}   
-d3.select('h2#data-title').text('All the Nobel-winners');  
+var loadCountryWinnersJSON = function(country){  
+	d3.json('data/winners_by_country/' + country + '.json',  function(error, data) {   
+	if(error)
+	{ console.log(error); }   
+	d3.select('h2#data-title').text('All the Nobel-winners from ' + country);  
+	d3.select('div#data pre').html(JSON.stringify(data, null, 4));  
+}); 
+};
 
-d3.select('div#data pre').html(JSON.stringify(data, null, 4)); 
-});
+loadCountryWinnersJSON('Russia');
